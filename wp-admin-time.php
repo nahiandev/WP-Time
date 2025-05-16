2234b1212eba5bf187aa8e637e514234db7c28b5
Ibne Nahian <evilprince2009@yahoo.com>
2025-05-16 19:09:19 +0600

    image

--- wp-admin-time.php
+++ wp-admin-time.php
@@ -5,7 +5,7 @@ Description: Shows current WordPress and server time in admin notices.
 Version: 1.0
 Author: Ibne Nahian (@nahiandev)
 Author URI: https://www.linkedin.com/in/nahiandev/
-Plugin URI: https://github.com/nahiandev/wp-time
+Plugin URI: https://github.com/nahiandev/WP-Time
 */
 
 
